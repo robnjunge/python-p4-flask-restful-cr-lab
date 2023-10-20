fdadde86c736b186a1b14e99037bf4ee7e32920c
robertnjunge <mubearobert@gmail.com>
2023-10-20 23:41:27 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -7,3 +7,19 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+
+class Newsletter(db.Model, SerializerMixin):
+    __tablename__ = 'newsletters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'body': self.body
+        }
